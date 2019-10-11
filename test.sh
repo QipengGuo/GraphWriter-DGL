006b484b2eeb540c0bd62969e8fdd6d43d613eb6
QipengGuo <qpguo16@fudan.edu.cn>
2019-10-11 20:01:09 +0000

    fix test script

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ env CUDA_VISIBLE_DEVICES=0 python -u train.py --share_vocab --save_model tmp_mod
 perl detokenizer.perl -l en < tmp_gold.txt > tmp_gold.txt.a
 perl detokenizer.perl -l en < tmp_pred.txt > tmp_pred.txt.a
 perl multi-bleu.perl tmp_gold.txt < tmp_pred.txt
-perl multi-bleu.perl tmp_gold.txt.a < tmp_pred.txt.a
+perl multi-bleu-detok.perl tmp_gold.txt.a < tmp_pred.txt.a
